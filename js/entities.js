4f6eb3325dbb33be49016eed475ec0735ffd5058
Based Race <clawrace1@gmail.com>
2026-03-01 05:47:41 +0700

    Update entities.js

--- js/entities.js
+++ js/entities.js
@@ -32,7 +32,7 @@ export class Racer {
     this.progress = 0;
     
     // Dynamic AI - competitive organic movement
-    this.baseSpeed = 40 + Math.random() * 1;
+    this.baseSpeed = 20 + Math.random() * 1;
     this.targetSpeed = this.baseSpeed;
     this.currentSpeed = this.baseSpeed;
     this.lastSpeedChange = 0;
@@ -81,7 +81,7 @@ export class Racer {
     this.currentSpeed += (this.targetSpeed - this.currentSpeed) * 0.08;
     
     // Y moves forward (toward finish line at Y=7450)
-    this.yPosOnScreen += this.currentSpeed * dt / 2500;
+    this.yPosOnScreen += this.currentSpeed * dt / 2000;
    
     // X calculated from Y using diagonal ratio from start points
     this.x = this.startX + ((this.yPosOnScreen - this.startY) * this.diagonalRatio);
@@ -107,7 +107,7 @@ export class Racer {
     this.progress = 0;
     
     // Reset dynamic AI - competitive
-    this.baseSpeed = 40 + Math.random() * 1;
+    this.baseSpeed = 20 + Math.random() * 1;
     this.targetSpeed = this.baseSpeed;
     this.currentSpeed = this.baseSpeed;
     this.sinOffset = Math.random() * Math.PI * 2;
