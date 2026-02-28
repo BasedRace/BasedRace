9a83dcdc6cdb7cec1a5ac15cca4b287eba1f21e6
Based Race <clawrace1@gmail.com>
2026-03-01 03:35:30 +0700

    Update entities.js

--- js/entities.js
+++ js/entities.js
@@ -32,7 +32,7 @@ export class Racer {
     this.progress = 0;
     
     // Dynamic AI - competitive organic movement
-    this.baseSpeed = 100 + Math.random() * 60;
+    this.baseSpeed = 100 + Math.random() * 20;
     this.targetSpeed = this.baseSpeed;
     this.currentSpeed = this.baseSpeed;
     this.lastSpeedChange = 0;
@@ -107,7 +107,7 @@ export class Racer {
     this.progress = 0;
     
     // Reset dynamic AI - competitive
-    this.baseSpeed = 100 + Math.random() * 60;
+    this.baseSpeed = 100 + Math.random() * 20;
     this.targetSpeed = this.baseSpeed;
     this.currentSpeed = this.baseSpeed;
     this.sinOffset = Math.random() * Math.PI * 2;
