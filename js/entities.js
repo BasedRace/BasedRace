ff8ca5463678ca018f0e280203427d56f7802f33
Based Race <clawrace1@gmail.com>
2026-02-28 12:48:17 +0700

    Update entities.js

--- js/entities.js
+++ js/entities.js
@@ -25,7 +25,7 @@ export class Track {
   generate() {
     this.tiles = [];
     let currentX = this.initialX;
-    let currentY = 0;
+    let currentY = -550;
     
     for (let i = 0; i < this.sequence.length; i++) {
       const assetName = this.sequence[i];
