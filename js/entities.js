1837cd0a0fb6095269ddb7d78340cbda80f010bf
Based Race Dev <dev@basedrace.game>
2026-03-01 02:03:19 +0000

    Set FINISH_DISTANCE to 202

--- js/entities.js
+++ js/entities.js
@@ -2,7 +2,7 @@
 
 // Global race distance
 const TOTAL_RACE_DISTANCE = 7500;
-const FINISH_DISTANCE = 190;
+const FINISH_DISTANCE = 202;
 
 // Racer class for autonomous 4-racer system
 export class Racer {
