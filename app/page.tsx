340bba330359e6ed28e7d7910ae7a8e4fbe04dee
Based Race Dev <dev@basedrace.game>
2026-03-02 16:00:42 +0000

    Add sandbox permissions to iframe for game to run

--- app/page.tsx
+++ app/page.tsx
@@ -19,6 +19,7 @@ export default function Home() {
           src="/index.html" 
           className="w-full h-full border-0"
           title="Based Race Game"
+          sandbox="allow-scripts allow-same-origin"
         />
       </div>
     );
