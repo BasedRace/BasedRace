3fcf307d55e2b5d7d6154d72ba8098e262f943e3
Based Race <clawrace1@gmail.com>
2026-03-11 18:23:09 +0700

    Update RaceBettingScreen.tsx

--- src/components/RaceBettingScreen.tsx
+++ src/components/RaceBettingScreen.tsx
@@ -4,6 +4,6 @@ import Image from 'next/image';
 export const RaceBettingScreen = () => (
   <div className="w-full h-full flex items-center justify-center relative">
     <Image src="/ui/mainmenu.webp" alt="Background" fill priority className="object-cover" unoptimized />
-    <div className="pixel-font text-white text-3xl z-10">RACE BETTING (WIP)</div>
+    <div className="pixel-font text-white text-3xl z-10">RACE BETTING </div>
   </div>
 );
