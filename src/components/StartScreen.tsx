59d7146aaa98bd9a3ca03bfcc5681feea2e88b84
d.mattrenggana <d.mattrenggana@gmail.com>
2026-03-12 22:49:53 +0000

    feat: update StartScreen button styles

--- src/components/StartScreen.tsx
+++ src/components/StartScreen.tsx
@@ -8,17 +8,17 @@ export const StartScreen = ({ onSelectTournament, onSelectRaceBetting }: {
 }) => (
   <div className="w-full h-full relative overflow-y-auto custom-scrollbar">
     <div 
-      className="min-h-full w-full flex flex-col items-center justify-center gap-16 p-10"
+      className="min-h-full w-full flex flex-col items-center justify-center gap-24 p-10"
     >
       <button 
         onClick={onSelectRaceBetting} 
-        className="pixel-font pixel-border w-[98%] max-w-4xl text-center pixel-btn transition-all duration-150 bg-[#e7f2eb] text-[#0f10f4] text-7xl py-48 sm:text-9xl sm:py-64 shrink-0"
+        className="pixel-font pixel-border w-[98%] max-w-4xl text-center pixel-btn transition-all duration-150 bg-[#e7f2eb] text-[#0f10f4] text-7xl py-56 sm:text-9xl sm:py-72 shrink-0"
       >
         RACE BETTING
       </button>
       <button 
         onClick={onSelectTournament} 
-        className="pixel-font pixel-border w-[98%] max-w-4xl text-center pixel-btn transition-all duration-150 bg-[#e7f2eb] text-[#0f10f4] text-7xl py-48 sm:text-9xl sm:py-64 shrink-0"
+        className="pixel-font pixel-border w-[98%] max-w-4xl text-center pixel-btn transition-all duration-150 bg-[#e7f2eb] text-[#0f10f4] text-7xl py-56 sm:text-9xl sm:py-72 shrink-0"
       >
         TOURNAMENT
       </button>
