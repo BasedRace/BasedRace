490a218c6058ef772117950b05dc3d3b30d80469
d.mattrenggana <d.mattrenggana@gmail.com>
2026-03-13 07:50:26 +0000

    feat: update racer starting coordinates

--- public/js/entities.js
+++ public/js/entities.js
@@ -18,10 +18,10 @@ export class Racer {
     
     // Starting positions from user
     const startPositions = [
-        { x: -90, y: -30 },
-        { x: 190, y: 150 },
-        { x: 460, y: 300 },
-        { x: 790, y: 530 }
+        { x: -80, y: -30 },
+        { x: 190, y: 160 },
+        { x: 490, y: 320 },
+        { x: 790, y: 550 }
     ];
     
     const pos = startPositions[laneIndex] || { x: 0, y: 0 };
@@ -85,10 +85,10 @@ export class Racer {
   // Reset to start
   reset() {
     const startPositions = [
-        { x: -90, y: -30 },
-        { x: 190, y: 150 },
-        { x: 460, y: 300 },
-        { x: 790, y: 530 }
+        { x: -80, y: -30 },
+        { x: 190, y: 160 },
+        { x: 490, y: 320 },
+        { x: 790, y: 550 }
     ];
     const pos = startPositions[this.laneIndex] || { x: 0, y: 0 };
     this.startX = pos.x;
